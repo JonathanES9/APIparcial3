a84d613a9954afcdec443e536bdaa91afa475fee
jermiasoliva0805 <jeremiasoliva0805@mi.unc.edu.ar>
2025-10-27 12:39:38 -0300

    cambie nombre carpeta

---
+++ API PARCIAL 3/API PARCIAL 3/DataTransferObjects/GuessNumberRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_PARCIAL_3.DataTransferObjects
+{
+    public class GuessNumberRequest
+    {
+        [Required(ErrorMessage = "El ID del juego es obligatorio.")]
+        public int GameId { get; set; }
+
+        [Required(ErrorMessage = "El número intentado es obligatorio.")]
+        [RegularExpression(@"^(?!.*(.).*\1)\d{4}$", ErrorMessage = "El número debe tener 4 dígitos únicos.")]
+        public string AttemptedNumber { get; set; } = string.Empty;
+    }
+}
