f4d50fb67d15f975bb990c986dda3863efcc8847
Mara Delgadillo <84547390+MaraD98@users.noreply.github.com>
2025-10-28 11:44:10 -0300

    Creacion de servicio y controller de attempt

--- API PARCIAL 3/API PARCIAL 3/DataTransferObjects/GuessNumberResponse.cs
+++ API PARCIAL 3/API PARCIAL 3/DataTransferObjects/GuessNumberResponse.cs
@@ -1,16 +1,11 @@
 ﻿
-
-
 namespace API_PARCIAL_3.DataTransferObjects
 {
     public class GuessNumberResponse
     {
-        //Response OK: { “gameid”: 1, “attemptedNumber”: 1234, “message”: “¡Felicidades! Has
-        //adivinado el número.”}
-        //Response NO OK: { “message”: “El juego 1 ya ha finalizado.” }
-        public int gameid { get; set; }
-        public int attemptedNumber { get; set; }
-        public string message { get; set; } = string.Empty;
+        public int Gameid { get; set; }
+        public string AttemptedNumber { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
 
     }
 }
