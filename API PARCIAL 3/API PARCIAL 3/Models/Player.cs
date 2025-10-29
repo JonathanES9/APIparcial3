067d9138e17491447ab41b4f5d2ac9c08b1fa37c
Mara Delgadillo <84547390+MaraD98@users.noreply.github.com>
2025-10-29 10:29:22 -0300

    actualizacion de edad

--- API PARCIAL 3/API PARCIAL 3/Models/Player.cs
+++ API PARCIAL 3/API PARCIAL 3/Models/Player.cs
@@ -15,6 +15,7 @@ namespace API_PARCIAL_3.Models
         public required string LastName { get; set; }
 
         [Required]
+        [Range(5, 120, ErrorMessage = "La edad debe ser de al menos 5 años.")]
         public required int Age { get; set; }
 
         public DateTime RegistrationDate { get; set; }
